0a3cb4a3b2d35b157bf6f77e908b8e81439cde62
Amos juda <amosjudasilva@gmail.com>
2023-08-02 15:45:33 -0300

    fileReader and BufferReader / block try-with-resourcesbest way

--- workingWithFiles/src/application/Program.java
+++ workingWithFiles/src/application/Program.java
@@ -1,27 +1,78 @@
 package application;
 
-import java.io.File;
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.IOException;
-import java.util.Scanner;
+
+//import java.io.File;
+//import java.io.IOException;
+//import java.util.Scanner;
 
 public class Program {
-
+// 1° class
+//	public static void main(String[] args) {
+//		// to read the content inside the file
+//		File file= new File("c:\\temp\\in.txt");
+//		Scanner sc = null;
+//		
+//		try {
+//			sc = new Scanner(file);
+//			while(sc.hasNextLine()) {
+//				System.out.println(sc.nextLine());
+//			}
+//		} catch(IOException e) {
+//			System.out.println("Error: " + e.getMessage());
+//		} finally {
+//			if(sc != null) {
+//				sc.close();
+//			}
+//		}
+//	}
+// 2° class	
+//	public static void main(String[] args) {
+//		String path = "c:\\temp\\in.txt";
+//		FileReader fr = null;
+//		BufferedReader br = null;
+//		
+//		try {
+//			fr = new FileReader(path);
+//			br = new BufferedReader(fr);
+//			
+//			String line = br.readLine();
+//			
+//			while(line != null) {
+//				System.out.println(line);
+//				line = br.readLine();
+//			}
+//		} catch(IOException e) {
+//			System.out.println("Error: " + e.getMessage());
+//		} finally {
+//			try {
+//				if(br != null) {
+//					br.close();
+//				}
+//				if(fr != null) {
+//					fr.close();
+//				}
+//			}
+//			catch(IOException e) {
+//				e.printStackTrace();
+//			}
+//		}
+//	}
+// 3° class
 	public static void main(String[] args) {
-		// to read the content inside the file
-		File file= new File("c:\\temp\\in.txt");
-		Scanner sc = null;
+		String path = "c:\\temp\\in.txt";
 		
-		try {
-			sc = new Scanner(file);
-			while(sc.hasNextLine()) {
-				System.out.println(sc.nextLine());
+		try(BufferedReader br = new BufferedReader(new FileReader(path))){
+			String line = br.readLine();
+			
+			while(line != null) {
+				System.out.println(line);
+				line = br.readLine();
 			}
 		} catch(IOException e) {
 			System.out.println("Error: " + e.getMessage());
-		} finally {
-			if(sc != null) {
-				sc.close();
-			}
-		}
+		} 
 	}
 }
