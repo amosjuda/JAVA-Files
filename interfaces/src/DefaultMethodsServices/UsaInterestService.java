70a34c471e6743537b8319a4e8b2c98e13f15698
Amos juda <amosjudasilva@gmail.com>
2023-08-24 15:55:21 -0300

    Default methods - interfaces with default method, implements and deletes - completed

--- interfaces/src/DefaultMethodsServices/UsaInterestService.java
+++ interfaces/src/DefaultMethodsServices/UsaInterestService.java
@@ -1,11 +1,12 @@
 package DefaultMethodsServices;
 
-public class BrazilInterestService implements InterestService{
+public class UsaInterestService implements InterestService{
 	private double interestRate;
 	
-	public BrazilInterestService(double interestRate) {
+	public UsaInterestService(double interestRate) {
 		this.interestRate = interestRate;
 	}
+	
 	@Override
 	public double getInterestRate() {
 		return interestRate;
